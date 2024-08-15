8b95706e978a26066b9a41c9d65fa84b9399e19a
Crusher2805 <151662737+Crusher2805@users.noreply.github.com>
2024-08-14 20:47:52 -0600

    Add files via upload

---
+++ Caso_2/src/caso_2/Caso_2.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package caso_2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Aulas Heredia
+ */
+public class Caso_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Hotel app = new Hotel();
+        boolean salir = false;
+
+        while (!salir) {
+            int opcion = Integer.parseInt(JOptionPane.showInputDialog(null,
+                    "Ingrese el número de la operación que desea realizar:\n"
+                    + "1. Modificar habitaciones\n"
+                    + "2. Mostrar habitaciones\n"
+                    + "3. Mostrar resumen\n"
+                    + "4. Salir"));
+
+            switch (opcion) {
+                case 1:
+                    app.modificar();
+                    break;
+                case 2:
+                    app.mostrar();
+                    break;
+                case 3:
+                    app.resumen();
+                    app.resumen1();
+                    break;
+                case 4:
+                    salir = true;
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción inválida");
+                    break;
+            }
+        }
+    }
+}
---
+++ Caso_2/src/caso_2/Hotel.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package caso_2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Aulas Heredia
+ */
+public class Hotel {
+
+    private String[] habitaciones = {"habitacion", "habitacion2", "habitacion3", "habitacion4", "habitacion5"};
+    private int[] pisos = {1, 2, 3, 4, 5};
+    private int[] codigo = {101, 202, 303, 404, 505};
+    private double[] precio = {50, 60, 40, 30, 20};
+    private String[] estado = {"libre", "sucia", "ocupada", "ocupada", "mantenimiento"};
+    private String[] tipo = {"simple", "doble", "grande", "premium", "extra_grande"};
+
+    public void mostrar() {
+        String info = "";
+        for (int i = 0; i < habitaciones.length; i++) {
+            info += "Habitacion: " + habitaciones[i] + "\n";
+            info += "Piso: " + pisos[i] + "\n";
+            info += "codigo: " + codigo[i] + "\n";
+            info += "precio por noche: " + precio[i] + "\n";
+            info += "estado: " + estado[i] + "\n";
+            info += "tipo: " + tipo[i] + "\n";
+            info += "\n";
+        }
+        JOptionPane.showMessageDialog(null, info);
+    }
+
+    public void resumen1() {
+        int suma = 0;
+        for (int i = 0; i < estado.length; i++) {
+            if (estado[i].equals("ocupada") || estado[i].equals("libre") || estado[i].equals("sucia") || estado[i].equals("libre")) {
+                suma += 1;
+            }
+            
+        }
+        JOptionPane.showMessageDialog(null, "Resumen de habitaciones " + suma);
+    }
+
+    public void resumen() {
+        double suma = 0;
+        for (int i = 0; i < precio.length; i++) {
+            if (estado[i].equals("ocupada")) {
+                suma += precio[i];
+
+            }
+        }
+        JOptionPane.showMessageDialog(null, "La ganancia de las habitaciones ocupadas es de " + suma + "$");
+    }
+
+    public void modificar() {
+        String nombreM = JOptionPane.showInputDialog(null, "Ingrese el nombre de la habitacion que desea modificar ");
+        int indice = -1;
+        for (int i = 0; i < habitaciones.length; i++) {
+            if (habitaciones[i].equalsIgnoreCase(nombreM)) {
+                indice = i;
+                break;
+            }
+        }
+        if (indice != -1) {
+            String nombreN = JOptionPane.showInputDialog(null, "Ingrese el nuevo nombre de la habitacion ");
+            int codigoN = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el nuevo codigo de la habitacion "));
+            String estadoN = JOptionPane.showInputDialog(null, "Ingrese el nuevo tipo de estado de la habitacion ");
+            String tipoN = JOptionPane.showInputDialog(null, "Ingrese el nuevo tipo de habitacion");
+            habitaciones[indice] = nombreN;
+            codigo[indice] = codigoN;
+            estado[indice] = estadoN;
+            tipo[indice] = tipoN;
+            JOptionPane.showMessageDialog(null, "Informacion actualizada ");
+        } else {
+            JOptionPane.showMessageDialog(null, "La habitacion no existe ");
+        }
+    }
+
+}
